813f401ff4d0e58b91f84462bceb14d2e37e24bc
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-06-05 14:03:49 -0700

    added files

---
+++ tech-news-jawa-api/src/main/java/com/technews/TechNewsJawaApiApplication.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNewsJawaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNewsJawaApiApplication.class, args);
+	}
+
+}
